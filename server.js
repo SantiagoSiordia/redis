ee3bdd38f8e93b5f40d219c2770bc6ede95c2c91
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-04-19 19:21:23 -0500

    feat: redis caching photos

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const redis = require("redis");
+
+const DEFAULT_EXPIRATION = 3600;
+
+const redisClient = redis.createClient();
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+(async () => {
+  await redisClient.connect();
+})();
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
+
+  const photos = await redisClient.get("photos");
+
+  if (photos != null) return res.json(JSON.parse(photos));
+
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/photos",
     { params: { albumId } }
   );
+
+  redisClient.setEx("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
 
   res.json(data);
 });
